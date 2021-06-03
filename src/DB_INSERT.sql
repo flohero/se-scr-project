c82a89af0900d94e5af974ed2d84d1ef16673861
florian.weingartshofer <florian.weingartshofer@tuta.io>
2021-06-03 11:37:55 +0200

    Update Insert Script

--- src/DB_INSERT.sql
+++ src/DB_INSERT.sql
@@ -3,5 +3,7 @@ use product_rating;
 insert into users (username, password) values ('Florian', 'Test');
 
 insert into products (userId, name, manufacturer, description) values (1, 'COVID-19 Vaccine', 'Pfitzer/Biontech', 'Best Covid Vaccine, 5G ready');
+insert into products (userId, name, manufacturer, description) values (1, 'COVID-19 Vaccine', 'Astra Zeneca', 'Guaranteed autism');
+insert into products (userId, name, manufacturer, description) values (1, 'COVID-19 Vaccine', 'Johnson & Johnson', 'Birds die, when taking in the Vaccine');
 
 commit;
