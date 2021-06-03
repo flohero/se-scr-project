28bfc66d8478cefbbbea6ad44eb4a0efab73bf4d
florian.weingartshofer <florian.weingartshofer@tuta.io>
2021-06-03 09:35:23 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use Infrastructure\Connection;
+
 spl_autoload_register(function ($class) {
     $file = __DIR__ . '/src/' . str_replace('\\', '/', $class) . '.php';
     if (file_exists($file)) {
@@ -12,9 +14,21 @@ spl_autoload_register(function ($class) {
 // === register services
 $sp = new \ServiceProvider();
 
+// --- Application
+$sp->register(\Application\Queries\ProductsQuery::class);
+
 // --- Infrastructure
 $sp->register(\Infrastructure\Session::class, isSingleton: true);
 $sp->register(\Application\Interfaces\Session::class, \Infrastructure\Session::class);
+
+$sp->register(Connection::class,
+    function () {
+        return new Connection("localhost", "root", "toor", "product_rating");
+    },
+    isSingleton: true);
+
+$sp->register(\Infrastructure\Repository::class);
+$sp->register(\Application\Interfaces\ProductRepository::class, \Infrastructure\Repository::class);
 
 // --- Presentation
 // MVC - Framework
@@ -25,6 +39,7 @@ $sp->register(Presentation\MVC\MVC::class, function () {
 //controllers
 $sp->register(Presentation\Controllers\Home::class);
 $sp->register(Presentation\Controllers\Login::class);
+$sp->register(Presentation\Controllers\Products::class);
 
 // === handle requests
 $sp->resolve(Presentation\MVC\MVC::class)->handleRequest($sp);
